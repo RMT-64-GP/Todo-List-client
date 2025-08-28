fea47d31beced4e879b6a02a424a77f6e3871606
Melvern-CK <chilanix@gmail.com>
2025-08-28 00:15:36 -0700

    .env.port

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const { createServer } = require("http")
 const { Server } = require("socket.io")
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 app.use(cors())
